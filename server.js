53cd7d43aab9d6d3f256fca751017e08a6b066c5
altavada <117209509+altavada@users.noreply.github.com>
2023-02-14 19:06:43 -0500

    creating POST route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+const fs = require('fs');
 const PORT = 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +18,29 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => res.json(db));
+
+app.post('/api/notes', async (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        }
+        const priorNotes = JSON.parse(await fs.readFileSync('./db/db.json'));
+        priorNotes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(priorNotes), (err) => 
+            err ? console.log(err) : res.json('./db/db.json'));
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting review');
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
